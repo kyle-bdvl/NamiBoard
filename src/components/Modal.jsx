c91cb16e74c0abeaf01a46be086dde8dbd5fd533
Kyle <kyle.boudville@gmail.com>
2025-05-26 23:59:48 +0800

    made the sidebar look prettier and fixed the modal being transparent

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Modal({ open, onClose, children }) {
   if (!open) return null;
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(59,130,246,0.3)]">
       <div className="bg-white rounded shadow-lg p-6 min-w-[300px] relative">
         <button
           className="absolute top-2 right-2 text-gray-400 hover:text-gray-700"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,26 @@
+import Button from './Buttons'
 export default function Sidebar({ startWorkFlow, workFlows, onSelectKanban }) {
-  
+
   const anchorClass = "block mb-4 text-gray-800 text-base no-underline";
   return (
-    <aside className="w-[250px] bg-white p-5 border-r border-gray-200 flex flex-col items-start">
+    <aside className="w-[250px] bg-blue-300 p-5 border-r border-gray-200 flex flex-col items-start">
       <div className="mb-8 flex flex-row gap-2 items-center">
         <h2 className="text-2xl font-bold">NamiBoard </h2>
         <img src="../src/assets/waveLogo.webp" alt="waveLogo" className="w-6 h-6" />
       </div>
       <h2 className="text-2xl">Your WorkFlows</h2>
       <div>
-        <button className="mt-2" onClick={startWorkFlow}>Add WorkFlow</button>
+        <Button row={true} onClick={startWorkFlow}>
+          Add WorkFlow
+          <img className="w-4 h-4" src="../src/assets/addIcon.png" alt="AddButton" />
+        </Button>
+
       </div>
-      {workFlows.length ==0 ? <ul></ul> :<ul className="mt-5 bg-stone-200 rounded-md flex flex-col px-2 py-2">
+      {workFlows.length == 0 ? <ul></ul> : <ul className="mt-5 bg-stone-200 rounded-md flex flex-col px-2 py-2">
         {workFlows.map((work) => {
           return (
             <li key={work.id} className="mt-2">
-              <button onClick={() => { onSelectKanban(work.id) }}>{work.title}</button>
+              <Button onClick={() => { onSelectKanban(work.id) }}>{work.title}</Button>
             </li>
           );
         })}
